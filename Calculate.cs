3d7ee206b587930bb15a6b1421774556249db5c8
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-16 10:11:39 +0530

    First commit +-/*

---
+++ Calculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Calculate
+    {
+        public void Add(double n1,double n2)
+        {
+            Console.WriteLine($"{n1} + {n2} = {(n1 + n2)}" );
+        }
+        public  void Sub(double n1, double n2)
+        {
+            Console.WriteLine($"{n1} - {n2} = {(n1 - n2)} " );
+        }
+        public  void Div(double n1, double n2)
+        {
+            Console.WriteLine($"{n1} / {n2} = {(n1 / n2)}" );
+        }
+        public  void Multi(double n1, double n2)
+        {
+            Console.WriteLine($"{n1} * {n2} = {(n1 * n2)}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculatorApp;
+
+Console.WriteLine("Calculator Results");
+
+double num1 = 25;
+double num2 = 50;
+Calculate cal =new Calculate();
+cal.Add(num1,num2);
+cal.Sub(num1, num2);
+cal.Multi(num1, num2);
+cal.Div(num1, num2);
